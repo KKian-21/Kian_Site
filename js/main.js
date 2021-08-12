c78050e0ceea60bd228ed98193b3ccb6c0217413
Kian Fetic <kian.fetic20@mail.com>
2021-08-12 16:43:13 +0200

    impl scroll effect

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const sr = ScrollReveal({
+    distance: '60px',
+    duration: 2800,
+    // reset: true,
+})
+
+
+sr.reveal(`.fullPageContent,
+            .serviceContent
+            `,{
+    origin: 'top',
+    interval: 100,
+})
+
+sr.reveal(`.uberContent, 
+.contentKursPic`,
+{
+    origin: 'left',
+})
+
+sr.reveal(`.contentKursPic`,{
+    origin: 'right',
+    interval: 100,
+
+
+// sr.reveal(`.about__img-overlay, 
+//            .video__content,
+//            .subscribe__form`,{
+//     origin: 'right',
+//     interval: 100,
+})
